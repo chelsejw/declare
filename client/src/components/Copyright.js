4e2e39d7e0101fd49f6d75ccfb33b910c1dff0fc
chelsejw <chelsejw@gmail.com>
2020-11-29 01:38:19 +0800

    tweak copyright

--- client/src/components/Copyright.js
+++ client/src/components/Copyright.js
@@ -5,10 +5,10 @@ export default function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link color="inherit" href="https://github.com/chelsejw">
+        Chelsea E.
       </Link>
-      {new Date().getFullYear()}
+      {" "} {new Date().getFullYear()}
       {"."}
     </Typography>
   );
