48f627b07b6dc892f2f25574a522f2357a79d346
chelsejw <chelsejw@gmail.com>
2020-11-28 21:26:38 +0800

    add instructions for app and allow user to test with either local mongodb or mongo atlas

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ An app to automatically send the GA declaration form every week at a specified t
 ## Technologies: Frontend
 - React
 - Material UI
+
+## Setup Instructions
+
+Depending on your package manager, install the required depencies in both apps. I used `Yarn` in development.
+
+Do `yarn install` OR `npm i` from both `/client` and `/api` folders to install the necessary dependencies.
+
+Create a `.env` in `/api` for the MongoDB connection, following `.env.example`. 
+
+To start either app, do `npm start` or `yarn start` from their respective root folders.
--- api/index.js
+++ api/index.js
@@ -1,39 +1,48 @@
-require('dotenv').config();
-const ENV = process.env
-const mongoose = require('mongoose')
+require("dotenv").config();
+const ENV = process.env;
+const mongoose = require("mongoose");
+const localMongo = `mongodb://localhost:27017/${ENV.DB_NAME}`;
+const mongoAtlas = `mongodb+srv://${ENV.DB_USER}:${ENV.DB_PASS}@${ENV.DB_HOST}/${ENV.DB_NAME}`;
+let usingLocalDB = ENV.IS_LOCAL_DB === "true" ? true : false;
+const UserController = require(`./controllers/UserController`);
 
-const mongoAtlas = `mongodb+srv://${ENV.DB_USER}:${ENV.DB_PASS}@${ENV.DB_HOST}/${ENV.DB_NAME}?retryWrites=true&w=majority`
-const UserController = require(`./controllers/UserController`)
-
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 4000;
 
-(()=> {
-  const cors = require('cors')
+(() => {
+  const cors = require("cors");
 
-  app.use(cors())
+  app.use(cors());
   app.use(express.json());
-  app.post('/exists', UserController.validateRegisteredUser);
-  app.post('/register', UserController.register);
-  app.post('/login', UserController.login);
+  app.post("/exists", UserController.validateRegisteredUser);
+  app.post("/register", UserController.register);
+  app.post("/login", UserController.login);
 
-  app.get('/', (_, res) => {
-    res.send('hel')
-  })
+  app.get("/", (_, res) => {
+    res.send("hel");
+  });
 
-
-  mongoose.connect(mongoAtlas, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(_ => {
+  mongoose
+    .connect(usingLocalDB ? localMongo : mongoAtlas, { // Will use different connection URI depending on env settings
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((_) => {
       // DB connected successfully
-      console.log('DB connection successful')
+      console.log("DB connection successful");
+      console.log(`Using ${usingLocalDB ? "local" : "atlas"} connection`);
 
       app.listen(process.env.PORT || port, () => {
-        console.log(`Declare app listening on port: ${port}`)
-      })
+        console.log(`Declare app listening on port: ${port}`);
+      });
     })
-    .catch(err => {
-      console.log(err)
-    })
+    .catch((err) => {
+      console.log(err);
+      console.log(
+        `Error while trying to connect to ${
+          usingLocalDB ? "local" : "atlas"
+        } connection`
+      );
+    });
 })();
-
