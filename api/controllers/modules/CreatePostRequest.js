3860f8d70dc6b6569788b58b77c2ca1db3cc2f89
chelsejw <chelsejw@gmail.com>
2020-11-29 11:46:18 +0800

    add function to shape the axios post request for google forms

---
+++ api/controllers/modules/CreatePostRequest.js
@@ -0,0 +1,48 @@
+const qs = require("qs");
+function createPostRequest(userType, email, GAEmail, fullName, mobile) {
+  const date = new Date();
+  const currentDay = date.getDate();
+  const currentMonth = date.getMonth() + 1;
+  const currentYear = date.getFullYear();
+  const studentForm = {
+    ["entry.1315738832_year"]: currentYear, // Year of submission
+    ["entry.1315738832_month"]: currentMonth, // Month of submission
+    ["entry.1315738832_day"]: currentDay, // Day of submission
+    ["entry.2139343058"]: "SEIF 3", // Cohort Name (e.g. SEIF 3, DSI 18)
+    ["entry.350879435"]: fullName, // Name as per NRIC
+    ["entry.1443611571"]: mobile, // Contact Number
+    ["entry.88756219"]: GAEmail, // Email used to sign with GA
+    ["emailAddress"]: email, // Email
+    ["entry.1464206896"]: "No",
+    ["entry.748573793"]: "No",
+    ["entry.1351260288"]: "None of the Above",
+    ["entry.1840918119"]: "No",
+    ["entry.472021917"]: "No",
+    ["entry.1296804660"]: "None of the above",
+    ["entry.1655462793"]: "No",
+    ["entry.941208881"]: "Yes, I understand.",
+    ["entry.330888046"]: "Yes, I understand.",
+  };
+
+  const testForm = {
+    ["entry.218682909"]: fullName,
+    ["entry.376316146_year"]: year,
+    ["entry.376316146_month"]: month,
+    ["entry.376316146_day"]: day,
+    ["entry.1944830854"]: "Yes",
+    ["emailAddress"]: email,
+  };
+
+  let formData, postEndpoint;
+
+  if (userType === "test") {
+    formData = qs.stringify(testForm);
+    postEndpoint = `https://docs.google.com/forms/u/0/d/e/1FAIpQLSdG6OmYlhYEqLjFH33nV4t14-C-mi01xQrvAjEDNWHysAeJRA/formResponse`;
+  } else {
+    formData = qs.stringify(studentForm);
+    postEndpoint = `https://docs.google.com/forms/u/2/d/e/1FAIpQLSfviLd5-9633aKQBAeVIS58rr0WCEO8irhjLYFHYgGBsN49iQ/formResponse`;
+  }
+  return axios.post(postEndpoint, formData).catch((err) => console.log(err));
+}
+
+module.exports = createPostRequest;
