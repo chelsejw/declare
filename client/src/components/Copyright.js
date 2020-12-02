a932643edd66c99fb19b23d59703a67f192857e9
chelsejw <chelsejw@gmail.com>
2020-12-02 22:56:22 +0800

    client - add href prop to FAQ Link so that it shows a pointer cursor

--- client/src/components/Copyright.js
+++ client/src/components/Copyright.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-import { Typography, Link, Modal } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Link } from "@material-ui/core";
 import FAQModal from './faq/FAQModal';
 
 export default function Copyright() {
@@ -19,7 +18,7 @@ export default function Copyright() {
         Github Repo
       </Link>
       {" | "}
-      <Link onClick={() => setOpen(true)} color="primary">
+      <Link href="#" onClick={() => setOpen(true)} color="primary">
         FAQ
       </Link>
       <div>
@@ -27,52 +26,6 @@ export default function Copyright() {
       open={open}
       onClose={() => setOpen(false)}
       />
-
-        {/* <Modal
-          open={open}
-          onClose={() => setOpen(false)}
-          aria-labelledby="simple-modal-title"
-          aria-describedby="simple-modal-description"
-        >
-          <div style={modalStyle} className={classes.paper}>
-            <Typography variant="body2" color="textSecondary" align="center">
-              <Typography variant="h4" color="primary" align="center">
-                Quick FAQ
-              </Typography>
-
-              <p>
-                <Typography
-                  variant="body2"
-                  display="inline"
-                  color="textPrimary"
-                >
-                  Who is the app for?{" "}
-                </Typography>
-                Currently, it only sends forms{" "}
-                <Typography
-                  variant="body2"
-                  display="inline"
-                  color="textPrimary"
-                >
-                  for GA Singapore students
-                </Typography>
-                .
-              </p>
-              <p>
-                Things taking a while to load? The API might be restarting after
-                being idle.
-              </p>
-              <p>
-                Forgot your password? The app has no feature to reset your
-                password at the moment LOL. Contact me to figure something out!
-              </p>
-              <p>
-                Can I configure the declaration fields? Not at the moment – but
-                it is a feature I will be working on.
-              </p>
-            </Typography>
-          </div>
-        </Modal> */}
       </div>
     </Typography>
   );
