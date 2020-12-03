cedfbca8c7a03447ded3ff44b47f88f2b98c64c2
chelsejw <chelsejw@gmail.com>
2020-12-03 19:48:25 +0800

    log date and time for testing

--- api/jobs/sendTestForms.js
+++ api/jobs/sendTestForms.js
@@ -1,10 +1,29 @@
-require("dotenv").config();
-const ENV = process.env;
 const CreatePostRequest = require("./modules/createPostRequest");
 const callRequestInBatches = require("./modules/callRequestInBatches");
 const connectToDBAndRun = require("./connectToDBAndRun");
 const UserModel = require("../models/user");
+const moment = require("moment");
+
+function getFormattedDate() {
+  var date = new Date();
+  var str =
+    date.getDate() +
+    "-" +
+    (date.getMonth() + 1) +
+    "-" +
+    date.getFullYear() +
+    ", " +
+    date.getHours() +
+    ":" +
+    date.getMinutes() +
+    ":" +
+    date.getSeconds();
+
+  return str;
+}
+
 const sendGoogleForms = async () => {
+  console.log(`The date and time is`, getFormattedDate());
   const activeUsers = await UserModel.find({ active: true });
   const allPostRequests = activeUsers.map((user) => {
     // Create the post request to submit the form for each active user.
