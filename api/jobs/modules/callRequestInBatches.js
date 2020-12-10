ddc8813d8f96aabeb1ff77a5b943f6db95610427
chelsejw <chelsejw@gmail.com>
2020-12-10 21:36:22 +0800

    add catch

--- api/jobs/modules/callRequestInBatches.js
+++ api/jobs/modules/callRequestInBatches.js
@@ -1,13 +1,16 @@
 const callRequestInBatches = async (arrayOfPromises, batchSize) => {
-  console.log(`Starting to execute`);
-  let batchNum = 0;
+  console.log(`Starting to execute`)
+  let batchNum = 0
   while (arrayOfPromises.length > 0) {
-    batchNum++;
-    console.log(`Starting on batch number ${batchNum}`);
-    const batch = arrayOfPromises.splice(0, batchSize);
-    await Promise.all(batch);
-    console.log(`Resolved batch ${batchNum}`);
+    batchNum++
+    console.log(`Starting on batch number ${batchNum}`)
+    const batch = arrayOfPromises.splice(0, batchSize)
+    await Promise.all(batch).catch((err) => {
+      console.log(`There was an error while sending the declaration requests.`)
+      console.log(err)
+    })
+    console.log(`Resolved batch ${batchNum}`)
   }
-};
+}
 
 module.exports = callRequestInBatches
