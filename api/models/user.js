55c155af41ecc098cd6e492ce8b0b8383f58e9bc
chelsejw <chelsejw@gmail.com>
2021-03-13 11:07:46 +0800

    api: make cohort field optional

--- api/models/user.js
+++ api/models/user.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   ga_email: { type: String, required: true },
   active: { type: Boolean, default: true }, // determines if server will automatically send requests,
   mobile: { type: String, required: true },
-  cohort: { type: String, required: true },
+  cohort: { type: String, default: '' },
   user_type: { type: String, enum: ['student', 'team'], required: true },
   last_declared: Date,
   password: { type: String, required: true },
